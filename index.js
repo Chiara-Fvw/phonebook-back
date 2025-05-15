13c9c39db739be2366fe2051b86b9ce32b430ed9
chiara <chiara.fvw@gmail.com>
2025-05-15 11:17:48 +0200

    added ESlint and corrected the errors - Exercise 3.22

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
-const express = require('express');
-const morgan = require('morgan');
-const Person = require('./models/person');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const Person = require('./models/person')
+const app = express()
 
 app.use(express.static('dist'))
-app.use(express.json());
+app.use(express.json())
 
-morgan.token('data', function (req, res) { 
-  return JSON.stringify(req.body); 
-});
+morgan.token('data', function (req, res) {
+  return JSON.stringify(req.body)
+})
 
 app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens.data(req, res)
-    ].join(' ')
-  })
-);
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.data(req, res)
+  ].join(' ')
+})
+)
 
 /* let entries = [
-  { 
+  {
     "id": "1",
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": "2",
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": "3",
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": "4",
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ] */
 
 app.get('/', (request, response) => {
-  response.send('<h1>Phone Book</h1>');
-});
+  response.send('<h1>Phone Book</h1>')
+})
 
 app.get('/api/persons', (request,response) => {
   Person.find({}).then(persons => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-      .then(person => {
-        if (person) {
-          response.json(person);
-        } else {
-          response.status(404).end();
-        }
-      })
-      .catch(err => next(err));
-});
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(err => next(err))
+})
 
 app.get('/info', (request,response, next) => {
   Person.countDocuments()
@@ -75,63 +75,63 @@ app.get('/info', (request,response, next) => {
         <p>Phonebook has info for ${result} people</p> 
         <p>${new Date()}</p>`)
     })
-    .catch(err => next(err));
-});
+    .catch(err => next(err))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch(err => next(err));
-});
+    .catch(err => next(err))
+})
 
 app.post('/api/persons', (request, response, next) => {
-  let data = request.body;
+  let data = request.body
   if (!data) return response
-                .status(400)
-                .json({error: 'Something went wrong.'});
-  
+    .status(400)
+    .json({ error: 'Something went wrong.' })
+
   if (!data.name) return response
-                .status(400)
-                .json({error: "The entry must have a name."});
-  
+    .status(400)
+    .json({ error: 'The entry must have a name.' })
+
   if (!data.number) return response
-                .status(400)
-                .json({error: 'Entries must have a number'});
+    .status(400)
+    .json({ error: 'Entries must have a number' })
 
-  Person.find({name: data.name})
-        .then(result => {
-          if (result.length > 0) return response.status(400).json({error: 'Name must be unique'});
+  Person.find({ name: data.name })
+    .then(result => {
+      if (result.length > 0) return response.status(400).json({ error: 'Name must be unique' })
 
-          const person = new Person({
-            name: data.name,
-            number: data.number
-          });
-          
-          return person.save()
-            .then(savedPerson => {
-              response.json(savedPerson);
-            })
-            .catch(err => next(err)); 
-        });
-});
+      const person = new Person({
+        name: data.name,
+        number: data.number
+      })
+
+      return person.save()
+        .then(savedPerson => {
+          response.json(savedPerson)
+        })
+        .catch(err => next(err))
+    })
+})
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'});
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message});
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
+  next(error)
 }
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-});
+})
--- models/person.js
+++ models/person.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 //MONGODB_URI="your_connection_string_here" npm run dev
 
-console.log('connecting to ', url);
+console.log('connecting to ', url)
 
 mongoose.connect(url)
-    .then(result => console.log('connected to MongoDB'))
-    .catch(err => console.log('error connecting to MongoDB: ', err.message));
+  .then(result => console.log('connected to MongoDB'))
+  .catch(err => console.log('error connecting to MongoDB: ', err.message))
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -22,20 +22,20 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function(num) {
-        return /^\d{2,3}-\d{4,}$/.test(num);
+        return /^\d{2,3}-\d{4,}$/.test(num)
       },
       message: props => `${props.value} not a valid phone number: ***-*****`
     },
     required: [true, 'User phone number is required']
   }
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-});
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
